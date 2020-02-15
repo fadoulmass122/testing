c92d2fd6831cd6cc6414528b541d6dff9e8b6f85
fadoulmass122 <38453906+fadoulmass122@users.noreply.github.com>
2020-02-15 18:11:12 +0200

    add new file

--- GpSystem/Program.cs
+++ GpSystem/Program.cs
@@ -16,7 +16,7 @@ namespace GpSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FRM-MAin());
         }
     }
 }
